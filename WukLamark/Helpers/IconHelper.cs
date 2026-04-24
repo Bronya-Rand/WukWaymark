70e46ead18c09e7a0a5d3b482fd5cdfe736ec387
Bronya-Rand <40864681+Bronya-Rand@users.noreply.github.com>
2026-04-24 09:51:19 -0500

    chore: drop cache as 'neligable' performance

--- WukLamark/Helpers/IconHelper.cs
+++ WukLamark/Helpers/IconHelper.cs
@@ -1,19 +1,13 @@
 using Dalamud.Interface.Textures.Internal;
 using Dalamud.Interface.Textures.TextureWraps;
-using System.Collections.Generic;
 using System.Numerics;
 
 namespace WukLamark.Helpers
 {
     public static class IconHelper
     {
-        private static readonly Dictionary<uint, Vector2> IconSizeCache = [];
         public static Vector2? GetIconSize(uint iconId)
         {
-            // Search cache first
-            if (IconSizeCache.TryGetValue(iconId, out var cachedSize))
-                return cachedSize;
-
             IDalamudTextureWrap? tex;
             try
             {
@@ -27,9 +21,6 @@ namespace WukLamark.Helpers
 
             var texSize = tex.Size;
             tex.Dispose();
-
-            // Cache the size for future lookups
-            IconSizeCache[iconId] = texSize;
             return texSize;
         }
     }
